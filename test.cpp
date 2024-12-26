e168c584a71999bda1635d26df145cdfcc746f41
Jacob Ismael <jacob.ismael@gmail.com>
2024-12-26 12:00:32 -0800

    LED test 2

--- test.cpp
+++ test.cpp
@@ -5,9 +5,9 @@
 #define YELLOW 2
 #define GREEN 3
 
-#define LED_B 13
-#define LED_G 19
-#define LED_R 26
+#define LED_B 13 // 
+#define LED_R 19 //
+#define LED_G 26
 
 #define PYRO_1 16
 #define PYRO_2 18
