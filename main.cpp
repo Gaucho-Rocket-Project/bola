bda65dc2108e8e448100a72624c11b42b202c2f9
gauchorocketproject <gauchorocketproject@gmail.com>
2024-12-31 16:24:17 -0800

    finished LED test

--- main.cpp
+++ main.cpp
@@ -3,45 +3,43 @@
 
 #include "constants.h"
 
+void setup() {
+  wiringPiSetupGpio();
 
+  // LED Pins
+  pinMode(LED_B, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
+
+  // PYRO channels
+  pinMode(PYRO_1, OUTPUT);
+  pinMode(PYRO_2, OUTPUT);
+  pinMode(PYRO_3, OUTPUT);
+}
 
 void parseLED(ushort state = 0) {
-    switch(state) {
-        case RED:
-            digitalWrite (LED_B, HIGH);
-            digitalWrite (LED_G, HIGH);
-            digitalWrite (LED_R, LOW);
-            break;
-        case YELLOW:
-            digitalWrite (LED_B, LOW);
-            digitalWrite (LED_G, LOW);
-            digitalWrite (LED_R, HIGH);
-            break;
-        case GREEN:
-            digitalWrite (LED_B, HIGH);
-            digitalWrite (LED_G, LOW);
-            digitalWrite (LED_R, HIGH);
-            break;
-        default:
-            digitalWrite (LED_B, HIGH);
-            digitalWrite (LED_G, HIGH);
-            digitalWrite (LED_R, HIGH);
-    }
+
+  switch (state) {
+  case RED:
+    digitalWrite(LED_B, HIGH);
+    digitalWrite(LED_G, HIGH);
+    digitalWrite(LED_R, LOW);
+    break;
+  case YELLOW:
+    digitalWrite(LED_B, HIGH);
+    digitalWrite(LED_G, LOW);
+    digitalWrite(LED_R, LOW);
+    break;
+  case GREEN:
+    digitalWrite(LED_B, HIGH);
+    digitalWrite(LED_G, LOW);
+    digitalWrite(LED_R, HIGH);
+    break;
+  default:
+    digitalWrite(LED_B, HIGH);
+    digitalWrite(LED_G, HIGH);
+    digitalWrite(LED_R, HIGH);
+  }
 }
 
-int main() {
-
-    wiringPiSetupGpio();
-
-    // LED Pins
-    pinMode(LED_B, OUTPUT);
-    pinMode(LED_G, OUTPUT);
-    pinMode(LED_B, OUTPUT);
-
-    // PYRO channels
-    pinMode(PYRO_1, OUTPUT);
-    pinMode(PYRO_2, OUTPUT);
-    pinMode(PYRO_3, OUTPUT);
-
-    return 0;
-}
+int main() { return 0; }
--- test.cpp
+++ test.cpp
@@ -1,51 +1,75 @@
 #include <iostream>
+#include <string>
+#include <unistd.h>
 #include <wiringPi.h>
 
 #include "constants.h"
 
-
-
 void parseLED(ushort state = 0) {
-    switch(state) {
-        case RED:
-            digitalWrite (LED_B, HIGH);
-            digitalWrite (LED_G, HIGH);
-            digitalWrite (LED_R, LOW);
-            break;
-        case YELLOW:
-            digitalWrite (LED_B, LOW);
-            digitalWrite (LED_G, LOW);
-            digitalWrite (LED_R, HIGH);
-            break;
-        case GREEN:
-            digitalWrite (LED_B, HIGH);
-            digitalWrite (LED_G, LOW);
-            digitalWrite (LED_R, HIGH);
-            break;
-        default:
-            digitalWrite (LED_B, HIGH);
-            digitalWrite (LED_G, HIGH);
-            digitalWrite (LED_R, HIGH);
-    }
+
+  switch (state) {
+  case RED:
+
+    std::cout << "R( " << LED_R << "): 0" << std::endl;
+    std::cout << "G( " << LED_G << "): 1" << std::endl;
+    std::cout << "B( " << LED_B << "): 1" << std::endl;
+
+    digitalWrite(LED_B, HIGH);
+    digitalWrite(LED_G, HIGH);
+    digitalWrite(LED_R, LOW);
+    break;
+  case YELLOW:
+
+    std::cout << "R( " << LED_R << "): 1" << std::endl;
+    std::cout << "G( " << LED_G << "): 0" << std::endl;
+    std::cout << "B( " << LED_B << "): 0" << std::endl;
+
+    digitalWrite(LED_B, HIGH);
+    digitalWrite(LED_G, LOW);
+    digitalWrite(LED_R, LOW);
+    break;
+  case GREEN:
+
+    std::cout << "R( " << LED_R << "): 1" << std::endl;
+    std::cout << "G( " << LED_G << "): 0" << std::endl;
+    std::cout << "B( " << LED_B << "): 1" << std::endl;
+
+    digitalWrite(LED_B, HIGH);
+    digitalWrite(LED_G, LOW);
+    digitalWrite(LED_R, HIGH);
+    break;
+  default:
+    digitalWrite(LED_B, HIGH);
+    digitalWrite(LED_G, HIGH);
+    digitalWrite(LED_R, HIGH);
+  }
 }
 
-int main() {
+void testLED() {
+  parseLED(RED);
+  usleep(1000000);
+  parseLED(YELLOW);
+  usleep(1000000);
+  parseLED(GREEN);
+  usleep(1000000);
+  parseLED();
+}
 
-    wiringPiSetupGpio();
+int main(int argc, char **argv) {
 
-    // LED Pins
-    pinMode(LED_B, OUTPUT);
-    pinMode(LED_G, OUTPUT);
-    pinMode(LED_B, OUTPUT);
+  wiringPiSetupGpio();
 
-    // PYRO channels
-    pinMode(PYRO_1, OUTPUT);
-    pinMode(PYRO_2, OUTPUT);
-    pinMode(PYRO_3, OUTPUT);
+  // LED Pins
+  pinMode(LED_B, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
 
-    // test
+  // PYRO channels
+  pinMode(PYRO_1, OUTPUT);
+  pinMode(PYRO_2, OUTPUT);
+  pinMode(PYRO_3, OUTPUT);
 
-    digitalWrite (LED_R, HIGH);
+  testLED();
 
-    return 0;
+  return 0;
 }
