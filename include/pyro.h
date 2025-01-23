7d7f6843d34a0d10ee19d0ee4ab1c7a680bcc332
Srihari Prazid <ninjacoffee231@gmail.com>
2025-01-22 19:49:32 -0800

    first commit after jacob's comments (srihari)

---
+++ include/pyro.h
@@ -0,0 +1,8 @@
+#ifndef PYRO_H
+#define PYRO_H
+
+void setupPyroChannel(int pinNumber);
+
+void changePyroChannelState(int pinNumber, bool state);
+
+#endif
--- src/pyro.cpp
+++ src/pyro.cpp
@@ -1,14 +1,11 @@
-#include <iostream>
+#include "pyro.h" c
 #include "wiringPi.h"
+#include <iostream>
 
-void setupPyroChannel(int pinNumber){
-    pinMode(pinNumber, OUTPUT);
-    
-}
-
-void changePyroChannelState(int pinNumber, bool state){
-    if(state)
-        digitalWrite(pinNumber, ON);
-    else
-        digitalWrite(pinNumber, OFF);
+void setupPyroChannel(int pinNumber) { pinMode(pinNumber, OUTPUT); }
+void changePyroChannelState(int pinNumber, bool state) {
+  if (state)
+    digitalWrite(pinNumber, HIGH);
+  else
+    digitalWrite(pinNumber, LOW);
 }
