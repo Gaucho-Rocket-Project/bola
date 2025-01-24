77f16bd87e3039cbb1dcf0123ba17b66f612bf6c
Chen Hui Lin <chenhuilin1337@gmail.com>
2025-01-23 18:53:50 -0800

    added some header files not sure if we need to include them or not

--- src/servo.cpp
+++ src/servo.cpp
@@ -1,7 +1,7 @@
-#include "pwm.h"
-#include "setup.h"
-#include "servo.h"
-
 #include <iostream>
+
+#include "constants.h"
+#include "servo.h"
+#include "setup.h"
 
 float pwmTranslate(double angle) { return (318.31 * angle + 1000); }
