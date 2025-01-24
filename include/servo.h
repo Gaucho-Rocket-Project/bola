97b63fb3a6a80c72624d96deb5505e95ea22c784
Chen Hui Lin <chenhuilin1337@gmail.com>
2025-01-23 18:36:59 -0800

    fixed PWM function and included guard for header file

---
+++ include/servo.h
@@ -0,0 +1,8 @@
+#ifndef PWM_TRANSLATE_H
+#define PWM_TRANSLATE_H
+
+#include <iostream>
+
+float pwmTranslate(double angle);
+
+#endif
--- src/servo.cpp
+++ src/servo.cpp
@@ -1,9 +1,7 @@
 #include "pwm.h"
 #include "setup.h"
+#include "servo.h"
+
 #include <iostream>
-
-float pwmTranslate(double angle);
-
-int main() { setup(); }
 
 float pwmTranslate(double angle) { return (318.31 * angle + 1000); }
