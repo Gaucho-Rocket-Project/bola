c32223fc85f38658af84f34366987f8ffade86a1
Jacob Ismael <jacob.ismael@gmail.com>
2025-02-03 11:42:07 -0800

    added function stubs

---
+++ include/util.h
@@ -0,0 +1,9 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include "constants.h"
+#include <iostream>
+
+Time calculateSecondMotorActivation(Time initial_burn_time, float motor_acceleration, float upward_drag_coefficient, float downward_drag_coefficient);
+
+#endif
---
+++ src/util.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <unistd.h>
+#include <cmath>
+#include <wiringPi.h>
+
+#include "constants.h"
+#include "util.h"
+
+// @param: intial_burn_time
+Time calculateSecondMotorActivation(Time initial_burn_time, float motor_acceleration, float upward_drag_coefficient, float downward_drag_coefficient) {
+    // @todo: write function
+}
