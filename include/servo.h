17362eaf20f8210eb7a3858b4490fded1bcb5bb4
Jovia Low <jovialow23@gmail.com>
2025-02-05 19:27:55 -0800

    Made pwm_write function

--- include/servo.h
+++ include/servo.h
@@ -1,9 +1,10 @@
-#ifndef PWM_H
-#define PWM_H
+#ifndef SERVO_H
+#define SERVO_H
 
 #include <iostream>
 
 // given desired angle, output PWM signal output
 float pwmOutput(double angle);
+double pwmWrite(double startTime, double endTime, int sampleRate);
 
 #endif
