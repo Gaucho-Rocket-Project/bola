62fdb20dd839e993a283573d76e9a5f5f310500b
John Chen <john_chen@ucsb.edu>
2025-02-12 19:48:20 -0800

    BMP581 driver files

--- include/kalmanfilter.h
+++ include/kalmanfilter.h
@@ -2,5 +2,12 @@
 #define KALMANFILTER_H
 
 #include <Eigen/Dense> // Eigen for matrix operations
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <cmath>
+#include "ICM20948Driver.h"
+#include "BMP390Driver.h"
 
 #endif // KALMANFILTER_H
+
