33ee71f20613cdcf7a483abad5090a05e3948d2b
akqwua <152116363+akqwua@users.noreply.github.com>
2025-03-05 00:42:17 -0800

    added test trigger_parachute function

--- include/statemachine.h
+++ include/statemachine.h
@@ -26,7 +26,8 @@ class sensor_trigger {
   bool _parachute_shunt;
 
 public:
-  sensor_trigger();
+  sensor_trigger(state_data &state);
+  state_data &_state;
 
   int trigger_landing_legs();
   int trigger_second_motor();
