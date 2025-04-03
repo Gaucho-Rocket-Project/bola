453386cb93cdd87610bf8bcba90dcc40cfd655df
Nathan Ngo <nathanngo011@gmail.com>
2025-04-02 21:04:25 -0700

    bro

--- include/logger.h
+++ include/logger.h
@@ -2,12 +2,13 @@
 #define LOGGER_H
 
 #include <iostream>
+#include <unistd.h>  // for usleep()
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <wiringPi.h>
+#include <wiringPi.h> // use wiringPi instead of defining functions
 
 #ifdef __cplusplus
 }
