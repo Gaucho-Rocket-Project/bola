1f91d179ba9fe09eadfab65129a79c65c91b12d9
Srihari Prazid <srihariprazid@gmail.com>
2025-05-10 16:29:59 -0700

    added test code

--- testFlash/testFlash.ino
+++ testFlash/testFlash.ino
@@ -10,26 +10,31 @@ void setup() {
   }
   Serial.println(":white_check_mark: LittleFS Mounted Successfully");
   // List files
-  Serial.println(">> Listing files in root directory:");
+  // Serial.println(">> Listing files in root directory:");
+  // File root = LittleFS.open("/");
+  // File file = root.openNextFile();
+  // while (file) {
+  //   Serial.print("File: ");
+  //   Serial.println(file.name());
+  //   file = root.openNextFile();
+  // }
+  // Try to open the test.csv file
   File root = LittleFS.open("/");
-  File file = root.openNextFile();
-  while (file) {
-    Serial.print("File: ");
-    Serial.println(file.name());
-    file = root.openNextFile();
+  while (File file = root.openNextFile()) {
+    Serial.printf("Found %s  (%u bytes)\n", file.name(), file.size());
+    file.close();
   }
-  // // Try to open the test.csv file
-  // File csvFile = LittleFS.open("/test.csv", FILE_READ);
-  // if (!csvFile) {
-  //   Serial.println(":x: Failed to open /test.csv");
-  //   return;
-  // }
-  // Serial.println(">> Contents of /test.csv:");
-  // while (csvFile.available()) {
-  //   String line = csvFile.readStringUntil('\n');
-  //   Serial.println(line);
-  // }
-  // csvFile.close();
+  File csvFile = LittleFS.open("/test.csv", FILE_READ);
+  if (!csvFile) {
+    Serial.println(":x: Failed to open /test.csv");
+    return;
+  }
+  Serial.println(">> Contents of /test.csv:");
+  while (csvFile.available()) {
+    String line = csvFile.readStringUntil('\n');
+    Serial.println(line);
+  }
+  csvFile.close();
 }
 void loop() {
   // nothing else to do
