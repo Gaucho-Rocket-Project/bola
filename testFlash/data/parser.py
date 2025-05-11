22a176b6f365fdd1327c0da52ec5f60b3f8993ba
Cheng <cxi@ucsb.edu>
2025-05-11 14:43:03 -0700

    force to decimal (31 precision)

--- testFlash/data/parser.py
+++ testFlash/data/parser.py
@@ -2,11 +2,22 @@ import polars as pl
 import numpy as np
 
 def to_cpp_array(name, arr, dtype="double"):
-    arr_str = ", ".join(map(str, arr))
+    # Format each value as a decimal (no scientific notation)
+    arr_str = ", ".join(f"{x:.31f}" for x in arr)
     return f"{dtype} {name}[] = {{ {arr_str} }};"
 
 def main():
-    df = pl.read_csv("/ESP32/testFlash/data/servo_test_data_-_strong_step_test_force.csv") # Use your actual file path (FULL PATH REQUIRED!)
+    df = pl.read_csv(
+        "/home/cheng/Downloads/Rocket/ESP32/testFlash/data/strong_step_test_force.csv",
+        schema_overrides={
+            "Time": pl.Float64,
+            "Roll": pl.Float64,
+            "Pitch": pl.Float64,
+            "Roll Correction": pl.Float64,
+            "Pitch Correction": pl.Float64,
+            "Height": pl.Float64,
+        }
+    )
     df = df.drop_nulls()
 
     header = "#pragma once\n\n"
